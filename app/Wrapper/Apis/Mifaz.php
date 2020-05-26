687aad822871e9621f8ad30b8efd1048df80bd60
Merlin Roth <mail@merlin-roth.de>
2020-05-26 15:26:06 +0200

    finish mifaz and add tests

--- app/Wrapper/Apis/Mifaz.php
+++ app/Wrapper/Apis/Mifaz.php
@@ -5,10 +5,13 @@ namespace App\Wrapper\Apis;
 
 use GuzzleHttp\Client;
 use GuzzleHttp\TransferStats;
+use Illuminate\Support\Collection;
+use Carbon\Carbon;
 
 class Mifaz
 {
     const API_ENDPOINT = 'https://api.mifaz.de/';
+    const DEFAULT_OPTIONS = ['radius' => 10, 'journeydate' => '', 'tolerance' => 0];
     private $client;
     private $lastResponse;
 
@@ -16,10 +19,14 @@ class Mifaz
         $this->client = resolve(Client::class, ['config' => ['base_uri' => self::API_ENDPOINT ]]);
     }
 
-    public function getEntries($startLat, $startLng, $endLat, $endLng)
+    public function getEntries($startLat, $startLng, $endLat, $endLng, $options = []): Collection
     {
+        $params = ['f' => 'getEntries', 'startlongitude' => $startLng, 'startlatitude' => $startLat, 'goallatitude' => $endLat, 'goallongitude' => $endLng];
+        $options = array_merge(self::DEFAULT_OPTIONS, $options);
+        $params =  array_filter(array_merge($options, $params), function($value) { return !is_null($value) && $value !== ''; });
+
         $response = $this->client->get('request/', [
-            'query' => ['f' => 'getEntries', 'startlongitude' => $startLng, 'startlatitude' => $startLat, 'goallatitude' => $endLat, 'goallongitude' => $endLng]
+            'query' => $params
         ]);
 
         $this->lastResponse = $response;
@@ -27,7 +34,7 @@ class Mifaz
         $content = (string) $response->getBody();
 
         if (!empty($content)) {
-            return json_decode($content, true)['entries'];
+            return collect(json_decode($content, true)['entries']);
         } else {
             return false;
         }
--- database/factories/SearchFactory.php
+++ database/factories/SearchFactory.php
@@ -4,6 +4,8 @@
 
 use App\GeoLocation;
 use App\Search;
+use App\SearchRadius;
+use App\TimeRange;
 use Carbon\Carbon;
 use Faker\Generator as Faker;
 use Ramsey\Uuid\Uuid;
@@ -20,18 +22,21 @@ $factory->define(Search::class, function (Faker $faker) {
         'organizations' => null,
         'availabilityStarts' => Carbon::now()->addDays(1),
         'availabilityEnds' => Carbon::now()->addDays(1),
-        'arrival' => Carbon::now()->addDays(1),
-        'departure' => Carbon::now()->addDays(1)
+        'arrival' => Carbon::now()->addDays(1)
     ];
 });
 
 $factory->afterMaking(Search::class, function (Search $search) {
     //$startPoint = factory(GeoLocation::class)->make();
-    $startPoint = new GeoLocation(['latitude' => 53.5511, 'longitude' => 9.9937]);
-    $endPoint = new GeoLocation(['latitude' => 52.522, 'longitude' => 13.410]);
+    $startPoint = new SearchRadius(['radius' => 10]);
+    $endPoint = new SearchRadius(['radius' => 10]);
     $id = Uuid::uuid4();
+
+    $startPoint->setAttribute('location', new GeoLocation(['latitude' => 53.5511, 'longitude' => 9.9937]));
+    $endPoint->setAttribute('location', new GeoLocation(['latitude' => 52.522, 'longitude' => 13.410]));
 
     $search->setAttribute('startPoint', $startPoint);
     $search->setAttribute('endPoint', $endPoint);
     $search->setAttribute('id', $id);
+    $search->setAttribute('departure', new TimeRange(['time' => Carbon::now()->addDays(1), 'toleranceInDays' => 1]));
 });
